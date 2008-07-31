47d60e2b9f8ddec18107cee21e046cd4df50fdef
Matt Colyer <matt@colyer.name>
2008-07-30 23:50:39 -0700

    Added documentation and licensing information.

--- src/AFC.h
+++ src/AFC.h
@@ -1,7 +1,21 @@
 /* 
  * AFC.h
  * Defines and structs and the like for the built-in AFC client
- * Written by FxChiP
+ * 
+ * Copyright (c) 2008 Zack C. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
  */
 
 #include "usbmux.h"
--- src/plist.h
+++ src/plist.h
@@ -1,6 +1,21 @@
-/* plist.h
+/*
+ * plist.h
  * contains structures and the like for plists
- * written by fxchip
+ *
+ * Copyright (c) 2008 Zack C. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
  */
 
 #ifndef PLIST_H
