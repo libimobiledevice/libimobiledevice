de4b279089d40131346cbd28e4f27b6e9716fab9
Paul Sladen <libiphone@paul.sladen.org>
2008-08-30 09:03:16 -0700

    Fixed permissions on directories.
    Signed-off-by: Matt Colyer <matt@colyer.name>

--- src/ifuse.c
+++ src/ifuse.c
@@ -57,7 +57,7 @@ static int ifuse_getattr(const char *path, struct stat *stbuf) {
 	if (!file){
 		res = -ENOENT;
 	} else {
-		stbuf->st_mode = file->type | 0644; // but we don't want anything on the iPhone executable, like, ever
+	  	stbuf->st_mode = file->type | (S_ISDIR(file->type) ? 0755 : 0644);
 		stbuf->st_size = file->size;
 		stbuf->st_blksize = 2048; // FIXME: Is this the actual block size used on the iPhone?
 		stbuf->st_blocks = file->blocks;
