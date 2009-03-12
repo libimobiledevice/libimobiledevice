201e1ff5bf2054fcb1c0cc3cd7ba1e229dbde3fa
Zach C <fxchip@gmail.com>
2009-03-11 22:04:38 -0700

    Fixes a compile error and two usbmux bugs
    Signed-off-by: Matt Colyer <matt@colyer.name>

--- src/AFC.h
+++ src/AFC.h
@@ -58,21 +58,34 @@ struct iphone_afc_file_int {
 
 enum {
 	AFC_ERROR = 0x00000001,
-	AFC_GET_INFO = 0x0000000a,
-	AFC_GET_DEVINFO = 0x0000000b,
-	AFC_LIST_DIR = 0x00000003,
-	AFC_MAKE_DIR = 0x00000009,
-	AFC_DELETE = 0x00000008,
-	AFC_TRUNCATE = 0x00000007,
-	AFC_RENAME = 0x00000018,
 	AFC_SUCCESS_RESPONSE = 0x00000002,
-	AFC_FILE_OPEN = 0x0000000d,
-	AFC_FILE_CLOSE = 0x00000014,
-	AFC_FILE_SEEK = 0x00000011,
-	AFC_FILE_TRUNCATE = 0x00000015,
-	AFC_FILE_HANDLE = 0x0000000e,
-	AFC_READ = 0x0000000f,
-	AFC_WRITE = 0x00000010
+	AFC_LIST_DIR = 0x00000003,      // ReadDir
+	// 0x00000004                   // ReadFile
+	// 0x00000005                   // WriteFile
+	// 0x00000006                   // WritePart
+	AFC_TRUNCATE = 0x00000007,      // Truncate
+	AFC_DELETE = 0x00000008,        // RemovePath
+	AFC_MAKE_DIR = 0x00000009,      // MakeDir
+	AFC_GET_INFO = 0x0000000a,      // GetFileInfo
+	AFC_GET_DEVINFO = 0x0000000b,   // GetDeviceInfo
+	// 0x0000000c  // same as 5, but writes to temp file, then renames it.
+	AFC_FILE_OPEN = 0x0000000d,     // FileRefOpen
+	AFC_FILE_HANDLE = 0x0000000e,   // _unknownPacket
+	AFC_READ = 0x0000000f,          // FileRefRead
+	AFC_WRITE = 0x00000010,         // FileRefWrite
+	AFC_FILE_SEEK = 0x00000011,     // FileRefSeek
+	AFC_FILE_TELL = 0x00000012,     // FileRefTell
+	// 0x00000013                   // _unknownPacket
+	AFC_FILE_CLOSE = 0x00000014,    // FileRefClose
+	AFC_FILE_TRUNCATE = 0x00000015, // FileRefSetFileSize (ftruncate)
+	// 0x00000016                   // SetFatalError
+	// 0x00000017                   // SetConnectionOptions
+	AFC_RENAME = 0x00000018,        // RenamePath
+	// 0x00000019                   // SetFSBlockSize (0x800000)
+	// 0x0000001A                   // SetBlockSize (0x800000)
+	AFC_FILE_LOCK = 0x0000001B,     // FileRefLock
+	AFC_MAKE_LINK = 0x0000001C      // MakeLink
 };
 
 uint32 iphone_afc_get_file_handle(iphone_afc_file_t file);
+
