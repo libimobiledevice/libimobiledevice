bb2ce5fe74136cb3ce304a31641fb1e1a60dace7
Martin Szulecki <opensuse@sukimashita.com>
2009-04-13 21:08:37 +0200

    Update msync.py example to latest Python API

--- dev/msync.py
+++ dev/msync.py
@@ -5,14 +5,14 @@ from libiphone.iPhone import *
 # get msync client
 def GetMobileSyncClient() :
     phone = iPhone()
-    if not phone.InitDevice() :
+    if not phone.init_device() :
         print "Couldn't find device, is it connected ?\n"
         return None
-    lckd = phone.GetLockdownClient()
+    lckd = phone.get_lockdown_client()
     if not lckd :
         print "Failed to start lockdown service.\n"
         return None
-    msync = lckd.GetMobileSyncClient()
+    msync = lckd.get_mobile_sync_client()
     if not msync :
         print "Failed to start mobilesync service.\n"
         return None
@@ -25,16 +25,16 @@ if not msync :
     exit(1)
 
 array = PListNode(PLIST_ARRAY)
-array.AddSubString("SDMessageSyncDataClassWithDevice")
-array.AddSubString("com.apple.Contacts");
-array.AddSubString("---");
-array.AddSubString("2009-01-13 22:25:58 +0100");
-array.AddSubUInt(106);
-array.AddSubString("___EmptyParameterString___");
+array.add_sub_string("SDMessageSyncDataClassWithDevice")
+array.add_sub_string("com.apple.Contacts");
+array.add_sub_string("---");
+array.add_sub_string("2009-01-13 22:25:58 +0100");
+array.add_sub_uint(106);
+array.add_sub_string("___EmptyParameterString___");
 
-msync.Send(array)
-array = msync.Receive()
-print array.ToXml()
+msync.send(array)
+array = msync.receive()
+print array.to_xml()
 
 
 
