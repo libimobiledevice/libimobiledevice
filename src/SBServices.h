c6982451d82c340ee8a57c76e39db160c625a1a3
Nikias Bassen <nikias@gmx.li>
2009-12-15 00:02:50 +0100

    Support for new SBServices interface
    This lockdown service has been introduced in firmware 3.1 and allows to
    re-arrange the Spr*ngboard icons from the computer.
    
    [#99 state:resolved]
    
    Signed-off-by: Matt Colyer <matt@colyer.name>

---
+++ src/SBServices.h
@@ -0,0 +1,33 @@
+/*
+ * SBServices.h
+ * SpringBoard Services header file.
+ *
+ * Copyright (c) 2009 Nikias Bassen, All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA 
+ */
+#ifndef ISBSERVICES_H
+#define ISBSERVICES_H
+
+#include <glib.h>
+
+#include "libiphone/sbservices.h"
+
+struct sbservices_client_int {
+	iphone_connection_t connection;
+	GMutex *mutex;
+};
+
+#endif
