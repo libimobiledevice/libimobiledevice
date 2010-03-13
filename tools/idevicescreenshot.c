3f77e8a823b592322408c9fbcc5f6621815e105f
Martin Szulecki <opensuse@sukimashita.com>
2010-03-13 20:44:32 +0100

    Add missing copyright header to idevicescreenshot tool

--- tools/idevicescreenshot.c
+++ tools/idevicescreenshot.c
@@ -1,3 +1,26 @@
+/**
+ * idevicescreenshot -- Gets a screenshot from a connected iPhone/iPod Touch
+ *
+ * Copyright (C) 2010 Nikias Bassen <nikias@gmx.li>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more profile.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 
+ * USA
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
