5f08413081ada86f55e5a4585a6635aa34b57ed4
Martin Szulecki <m.szulecki@mirell.de>
2010-09-25 19:47:25 +0200

    Try to use glibtoolize if possible in autogen.sh to fix OSX build

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+gprefix=`which glibtoolize 2>&1 >/dev/null`
+if [ $? -eq 0 ]; then 
+  glibtoolize --force
+else
+  libtoolize --force
+fi
 aclocal -I m4
-libtoolize
 autoheader
 automake --add-missing
 autoconf
