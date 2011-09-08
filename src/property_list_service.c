e8e41ff405522f52a07deb9ea4482fcadf0d6e95
Nikias Bassen <nikias@gmx.li>
2011-09-08 16:41:49 +0200

    Include config.h if HAVE_CONFIG_H

--- src/property_list_service.c
+++ src/property_list_service.c
@@ -18,6 +18,9 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA 
  */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 #include <stdlib.h>
 #include <string.h>
 
