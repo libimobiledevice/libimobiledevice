a6be9013acc0f3f2ab37b703798e76409a1c7253
Nikias Bassen <nikias@gmx.li>
2012-01-20 00:18:42 +0100

    fix invalid characters in xml plists

--- src/property_list_service.c
+++ src/property_list_service.c
@@ -245,9 +245,9 @@ static property_list_service_error_t internal_plist_receive_timeout(property_lis
 			if (!memcmp(content, "bplist00", 8)) {
 				plist_from_bin(content, pktlen, plist);
 			} else {
-				/* iOS 4.3 hack: plist data might contain invalid null characters, thus we convert those to spaces */
+				/* iOS 4.3+ hack: plist data might contain invalid characters, thus we convert those to spaces */
 				for (bytes = 0; bytes < pktlen-1; bytes++) {
-					if (content[bytes] == 0x0)
+					if ((content[bytes] < 0x20) && (content[bytes] != 0x09) && (content[bytes] != 0x0a) && (content[bytes] != 0x0d))
 						content[bytes] = 0x20;
 				}
 				plist_from_xml(content, pktlen, plist);
