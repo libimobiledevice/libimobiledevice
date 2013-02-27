caacadcb26f1d60151040c5e83baed731479d4f8
Martin Szulecki <opensuse@sukimashita.com>
2013-02-27 16:27:18 +0100

    heartbeat: Fix compile time warning by casting returned error code

--- src/heartbeat.c
+++ src/heartbeat.c
@@ -113,7 +113,7 @@ heartbeat_error_t heartbeat_client_new(idevice_t device, lockdownd_service_descr
 heartbeat_error_t heartbeat_client_start_service(idevice_t device, heartbeat_client_t * client, const char* label)
 {
 	heartbeat_error_t err = HEARTBEAT_E_UNKNOWN_ERROR;
-	service_client_factory_start_service(device, HEARTBEAT_SERVICE_NAME, (void**)client, label, SERVICE_CONSTRUCTOR(heartbeat_client_new), &err);
+	service_client_factory_start_service(device, HEARTBEAT_SERVICE_NAME, (void**)client, label, SERVICE_CONSTRUCTOR(heartbeat_client_new), (uint16_t*)&err);
 	return err;
 }
 
