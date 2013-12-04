8238bdb59ff99deedec4da7daf6dfa54366dfcac
Aaron Burghardt <aburgh@mac.com>
2013-12-04 18:30:23 -0500

    userpref.c: removed excessive plist_free of a plist dict item.

--- common/userpref.c
+++ common/userpref.c
@@ -327,7 +327,6 @@ static int internal_get_value(const char* config_file, const char *key, plist_t 
 		plist_t n = plist_dict_get_item(config, key);
 		if (n) {
 			*value = plist_copy(n);
-			plist_free(n);
 			n = NULL;
 		}
 	}
