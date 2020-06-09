8cf167710b6add2bcbbb1099cb5fe72b5199f891
Martin Szulecki <martin.szulecki@libimobiledevice.org>
2020-06-09 03:04:31 +0200

    Apply changes towards a project wide common "autogen.sh" file

--- autogen.sh
+++ autogen.sh
@@ -4,20 +4,22 @@ olddir=`pwd`
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-cd "$srcdir"
+(
+  cd "$srcdir"
 
-gprefix=`which glibtoolize 2>&1 >/dev/null`
-if [ $? -eq 0 ]; then
-  glibtoolize --force
-else
-  libtoolize --force
-fi
-aclocal -I m4
-autoheader
-automake --add-missing
-autoconf
+  gprefix=`which glibtoolize 2>&1 >/dev/null`
+  if [ $? -eq 0 ]; then
+    glibtoolize --force
+  else
+    libtoolize --force
+  fi
+  aclocal -I m4
+  autoheader
+  automake --add-missing
+  autoconf
 
-cd "$olddir"
+  cd "$olddir"
+)
 
 if [ -z "$NOCONFIGURE" ]; then
   $srcdir/configure "$@"
