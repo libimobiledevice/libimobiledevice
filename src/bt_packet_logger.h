2bfc02c121e2ed0341e3d697b46335ace0f8415d
doggkruse <6468053+doggkruse@users.noreply.github.com>
2021-03-06 16:56:54 -0800

    Initial commit of working packet logger (idevicebtlogger)

---
+++ src/bt_packet_logger.h
@@ -0,0 +1,36 @@
+/*
+ * bt_packet_logger.h
+ * com.apple.bt_packet_logger service header file.
+ *
+ * Copyright (c) 2021 Geoffrey Kruse, All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _SYSLOG_RELAY_H
+#define _SYSLOG_RELAY_H
+
+#include "libimobiledevice/bt_packet_logger.h"
+#include "service.h"
+#include "common/thread.h"
+
+struct bt_packet_logger_client_private {
+	service_client_t parent;
+	THREAD_T worker;
+};
+
+void *bt_packet_logger_worker(void *arg);
+
+#endif
